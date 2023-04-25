27783647811a40c235f20ddf1a6202466bff48a7
Loran Kloeze <loran@freedomnet.nl>
2023-04-25 09:39:54 +0200

    New style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif'],
+    },
   },
   variants: {
     extend: {},
