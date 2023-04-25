06f7427ce13fff454a0f4a0d481118ac1e5ebd45
Loran Kloeze <loran@freedomnet.nl>
2023-04-25 09:50:22 +0200

    Mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'sans': ['Inter', 'sans-serif'],
+      'sans': ['Roboto', 'sans-serif'],
     },
   },
   variants: {
